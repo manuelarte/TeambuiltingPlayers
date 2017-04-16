03d904b9fdf09984634a285dec1aeac9161265b3
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-16 21:36:04 +0200

    TeamBuilting: Adding ports for microservices

--- src/main/java/org/manuel/teambuilting/players/controllers/PlayerToTeamController.java
+++ src/main/java/org/manuel/teambuilting/players/controllers/PlayerToTeamController.java
@@ -1,18 +1,22 @@
 package org.manuel.teambuilting.players.controllers;
 
+import org.manuel.teambuilting.players.model.entities.Player;
 import org.manuel.teambuilting.players.model.entities.PlayerToTeam;
 import org.manuel.teambuilting.players.services.command.PlayerToTeamCommandService;
 import org.manuel.teambuilting.players.services.query.PlayerToTeamQueryService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
 import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/players/{playerId}/teams")
+@RequestMapping("/playersToTeams")
 public class PlayerToTeamController {
 
 	private final PlayerToTeamQueryService playerToTeamQueryService;
@@ -25,23 +29,27 @@ public class PlayerToTeamController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public Collection<PlayerToTeam> findPlayerHistory(@PathVariable("playerId") final BigInteger playerId) {
+	public Collection<PlayerToTeam> findPlayerHistory(@RequestParam(value = "playerId", defaultValue = "") final BigInteger playerId) {
 		Assert.notNull(playerId);
 		return playerToTeamQueryService.findByPlayerId(playerId);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
-	public PlayerToTeam savePlayerToTeam(@PathVariable("playerId") final BigInteger playerId,
-			@Valid @RequestBody final PlayerToTeam playerToTeam) {
+	public PlayerToTeam savePlayerToTeam(@Valid @RequestBody final PlayerToTeam playerToTeam) {
 		Assert.notNull(playerToTeam);
-		Assert.isTrue(playerToTeam.getPlayerId().equals(playerId));
 		return playerToTeamCommandService.save(playerToTeam);
 	}
 
 	@RequestMapping(value = "/{playerToTeamId}", method = RequestMethod.DELETE, produces = "application/json")
-	public void deletePlayerToTeam(@PathVariable("playerId") final BigInteger playerId,
-										   @PathVariable("playerToTeamId") final BigInteger playerToTeamId) {
+	public void deletePlayerToTeam(@PathVariable("playerToTeamId") final BigInteger playerToTeamId) {
 		playerToTeamCommandService.delete(playerToTeamId);
+	}
+
+	@RequestMapping(path = "/teams/{teamId}/", method = RequestMethod.GET)
+	public Set<Player> getPlayersForTeam(@PathVariable("teamId") final String teamId,
+										 @RequestParam(value = "date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final LocalDate date) {
+		Assert.hasLength(teamId);
+		return playerToTeamQueryService.getPlayersFor(teamId, date);
 	}
 
 }
