780d4809c755bf8520e8c0df78adab94c276e084
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-24 15:52:53 +0200

    TeamBuilting: Changing the messages routing key

--- src/main/java/org/manuel/teambuilting/players/TeamBuiltingPlayersApplication.java
+++ src/main/java/org/manuel/teambuilting/players/TeamBuiltingPlayersApplication.java
@@ -1,7 +1,5 @@
 package org.manuel.teambuilting.players;
 
-import javax.inject.Inject;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
