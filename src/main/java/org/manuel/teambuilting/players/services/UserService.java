f7fdb9394df04f0139e3d3a0f08cb5387e66ed6c
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-24 09:56:08 +0200

    TeamBuilting: Changing the messages routing key

--- src/main/java/org/manuel/teambuilting/players/services/UserService.java
+++ src/main/java/org/manuel/teambuilting/players/services/UserService.java
@@ -1,40 +1,56 @@
 package org.manuel.teambuilting.players.services;
 
+import com.auth0.authentication.result.UserProfile;
+import com.auth0.spring.security.api.Auth0JWTToken;
+import lombok.AllArgsConstructor;
+import org.manuel.teambuilting.players.config.Auth0Client;
+import org.manuel.teambuilting.players.model.entities.Player;
 import org.manuel.teambuilting.players.model.entities.UserData;
+import org.manuel.teambuilting.players.repositories.PlayerRepository;
 import org.manuel.teambuilting.players.repositories.UserDataRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import javax.inject.Inject;
-import java.math.BigInteger;
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 /**
  * @author Manuel on 11/12/2016.
  */
 @Service
+@AllArgsConstructor
 public class UserService {
 
     private final UserDataRepository repository;
-
-    @Inject
-    public UserService(final UserDataRepository repository) {
-        this.repository = repository;
-    }
+    private Auth0Client auth0Client;
+    private PlayerRepository playerRepository;
 
     public UserData getOrCreateUserData(final String userId) {
         Assert.hasLength(userId);
         final Optional<UserData> retrieved = getUserData(userId);
-        return retrieved.isPresent() ? retrieved.get() : createUserData(userId, null);
+        return retrieved.isPresent() ? retrieved.get() : createUserData(userId);
     }
 
     private Optional<UserData> getUserData(final String userId) {
         return Optional.ofNullable(repository.findOne(userId));
     }
 
-    private UserData createUserData(final String userId, final BigInteger playerId) {
-        final UserData userData = new UserData(userId, playerId);
+    @Transactional
+    private UserData createUserData(final String userId) {
+        final Player player = createPlayer();
+        final UserData userData = new UserData(userId, player.getId());
         return repository.save(userData);
+    }
+
+    private Player createPlayer() {
+        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        final UserProfile user = auth0Client.getUser((Auth0JWTToken) auth);
+        final Player player = Player.builder().name(user.getName()).nickname(user.getNickname())
+                .imageLink(user.getPictureURL()).build();
+        playerRepository.save(player);
+        return player;
     }
 
     public UserData update(final UserData userData) {
