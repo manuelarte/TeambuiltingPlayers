31ae8bf5a818aa9ab5146f1d7eaf7e1c55d23046
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-05-14 16:32:08 +0200

    TeamBuiltingPlayers: Fixing one test, adding a save test

--- src/integrationTest/java/org/manuel/teambuilting/players/controllers/PlayerToTeamControllerTest.java
+++ src/integrationTest/java/org/manuel/teambuilting/players/controllers/PlayerToTeamControllerTest.java
@@ -2,7 +2,9 @@ package org.manuel.teambuilting.players.controllers;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.manuel.teambuilting.players.model.entities.Player;
@@ -10,6 +12,12 @@ import org.manuel.teambuilting.players.model.entities.PlayerToTeam;
 import org.manuel.teambuilting.players.repositories.PlayerRepository;
 import org.manuel.teambuilting.players.repositories.PlayerToTeamRepository;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,12 +25,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -46,6 +57,11 @@ public class PlayerToTeamControllerTest {
     private PlayerToTeamRepository playerToTeamRepository;
 
     private MockMvc mvc;
+
+    @BeforeClass
+    public static void beforeClass() {
+        setSecurityContext();
+    }
 
     @Before
     public void setup() {
@@ -90,10 +106,74 @@ public class PlayerToTeamControllerTest {
 
         final String contentAsString = mvc.perform(get("/playersToTeams/teams/teamId", "")).andExpect(status().isOk()).andReturn().getResponse()
                 .getContentAsString();
-        final List<Player> actual = mapper.readValue(contentAsString, new TypeReference<List<Player>>(){});
+        final List<PlayerToTeam> actual = mapper.readValue(contentAsString, new TypeReference<List<PlayerToTeam>>(){});
 
         assertTrue(actual.size() == 1);
-        assertEquals(player, actual.get(0));
+        assertEquals(playerToTeam, actual.get(0));
+    }
+
+    @Test
+    public void testSavePlayerToTeam() throws Exception {
+        final Player player = Player.builder().name("name").build();
+        playerRepository.save(player);
+        final PlayerToTeam playerToTeam = PlayerToTeam.builder().playerId(player.getId()).teamId("teamId").fromDate(new Date()).build();
+        final String contentAsString = mvc.perform(post("/playersToTeams", "").contentType(MediaType.APPLICATION_JSON).content(asJsonString(playerToTeam))).andExpect(status().isOk()).andReturn().getResponse()
+                .getContentAsString();
+    }
+
+    @SneakyThrows
+    private static String asJsonString(final Object obj) {
+        mapper.findAndRegisterModules();
+        return mapper.writeValueAsString(obj);
+    }
+
+    private static void setSecurityContext() {
+        final SecurityContext securityContext = new SecurityContext() {
+            @Override
+            public Authentication getAuthentication() {
+                return new Authentication() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return Arrays.asList(new SimpleGrantedAuthority("user"), new SimpleGrantedAuthority("admin"));
+                    }
+
+                    @Override
+                    public Object getCredentials() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getDetails() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getPrincipal() {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isAuthenticated() {
+                        return true;
+                    }
+
+                    @Override
+                    public void setAuthenticated(final boolean isAuthenticated) throws IllegalArgumentException {
+
+                    }
+
+                    @Override
+                    public String getName() {
+                        return null;
+                    }
+                };
+            }
+
+            @Override
+            public void setAuthentication(final Authentication authentication) {
+            }
+        };
+        SecurityContextHolder.setContext(securityContext);
     }
 
 }
