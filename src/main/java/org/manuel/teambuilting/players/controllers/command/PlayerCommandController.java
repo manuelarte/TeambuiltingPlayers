d58eaf69c52480c2b6d8e3002659a764001bbeb8
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-05-20 20:41:25 +0200

    TeamBuiltingPlayers: Fixing one test, adding a save test

--- src/main/java/org/manuel/teambuilting/players/controllers/command/PlayerCommandController.java
+++ src/main/java/org/manuel/teambuilting/players/controllers/command/PlayerCommandController.java
@@ -5,6 +5,7 @@ import org.manuel.teambuilting.exceptions.ValidationRuntimeException;
 import org.manuel.teambuilting.players.model.entities.Player;
 import org.manuel.teambuilting.players.services.command.PlayerCommandService;
 import org.manuel.teambuilting.players.services.geocoding.PlayerGeocodingService;
+import org.manuel.teambuilting.players.services.query.PlayerQueryService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,11 +20,12 @@ import java.util.Optional;
 public class PlayerCommandController {
 
 	private final PlayerCommandService playerCommandService;
+	private final PlayerQueryService playerQueryService;
 	private final PlayerGeocodingService playerGeocodingService;
 
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT}, produces = "application/json")
 	public Player updatePlayer(@Valid @RequestBody final Player player) {
-		if (player.getId() == null) {
+		if (player.getId() == null || !playerQueryService.findOne(player.getId()).isPresent()) {
 			throw new ValidationRuntimeException("Players cannot be created with a rest endpoint",
 					"Players cannot be created with a rest endpoint");
 		}
