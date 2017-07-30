96dfc73e8e72de1dc2d00f31535af45a223185bd
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-30 15:07:27 +0200

    Sharing Messages library

--- src/main/java/org/manuel/teambuilting/players/controllers/PlayerToTeamController.java
+++ src/main/java/org/manuel/teambuilting/players/controllers/PlayerToTeamController.java
@@ -8,7 +8,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
 import javax.validation.Valid;
 import java.math.BigInteger;
 import java.util.Collection;
@@ -21,30 +20,29 @@ public class PlayerToTeamController extends AbstractQueryController<PlayerToTeam
 
 	private final PlayerToTeamCommandService playerToTeamCommandService;
 
-	@Inject
 	public PlayerToTeamController(final PlayerToTeamQueryService playerToTeamQueryService, final PlayerToTeamCommandService playerToTeamCommandService) {
 		super(playerToTeamQueryService);
 		this.playerToTeamCommandService = playerToTeamCommandService;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public Collection<PlayerToTeam> findPlayerHistory(@RequestParam(value = "playerId", defaultValue = "") final BigInteger playerId) {
 		Assert.notNull(playerId, "The playerId cannot be null");
 		return queryService.findByPlayerId(playerId);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	@PostMapping(produces = "application/json")
 	public PlayerToTeam savePlayerToTeam(@Valid @RequestBody final PlayerToTeam playerToTeam) {
 		Assert.notNull(playerToTeam, "The entry cannot be null");
 		return playerToTeamCommandService.save(playerToTeam);
 	}
 
-	@RequestMapping(value = "/{playerToTeamId}", method = RequestMethod.DELETE, produces = "application/json")
+	@DeleteMapping(value = "/{playerToTeamId}", produces = "application/json")
 	public void deletePlayerToTeam(@PathVariable("playerToTeamId") final BigInteger playerToTeamId) {
 		playerToTeamCommandService.delete(playerToTeamId);
 	}
 
-	@RequestMapping(path = "/teams/{teamId}", method = RequestMethod.GET)
+	@GetMapping(path = "/teams/{teamId}")
 	public Set<PlayerToTeam> getPlayersForTeam(@PathVariable final String teamId,
 										 @RequestParam(value = "date", required = false, defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") final Date date) {
 		Assert.hasLength(teamId, "The teamId cannot be null");
