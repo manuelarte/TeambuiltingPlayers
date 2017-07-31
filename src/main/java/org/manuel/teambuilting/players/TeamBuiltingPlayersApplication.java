2cd3f0c1363dba2cd54b6a72ed0e93c73c06f79d
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-31 11:14:53 +0200

    Sharing core

--- src/main/java/org/manuel/teambuilting/players/TeamBuiltingPlayersApplication.java
+++ src/main/java/org/manuel/teambuilting/players/TeamBuiltingPlayersApplication.java
@@ -1,16 +1,13 @@
 package org.manuel.teambuilting.players;
 
-import org.manuel.teambuilting.core.config.CoreConfig;
 import org.manuel.teambuilting.core.config.EnableCoreFunctionalities;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableCoreFunctionalities
-@Import({CoreConfig.class})
 public class TeamBuiltingPlayersApplication {
 
 	public static void main(final String[] args) {
